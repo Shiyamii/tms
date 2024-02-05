314a91cb591d502926f9470847467d615038c30f
Tamas Palotas <tamas.palotas@etu.univ-cotedazur.fr>
2024-02-05 14:17:38 +0100

    Setup tms class

--- src/tms.py
+++ src/tms.py
@@ -1,132 +1,133 @@
 import re
 
-from src.interface import Interface
+from src.abstract_interface import AbstractInterface
 from src.ticket import Ticket
 from src.constants import State, Responsible, Type
 
-interface = Interface()
 
+class TMS:
+    def __init__(self, abstract_interface: AbstractInterface):
+        self.backlog = []
+        self.closed_backlog = []
+        self.interface = abstract_interface
 
-def close_ticket(case_id, case_list: list[Ticket], closed_list: list[Ticket]):
-    interface.print_close_ticket(case_id)
-    for case in case_list:
-        if case.id == case_id:
-            if case.responsible != Responsible.L1:
-                interface.print_l1_close_ticket()
-                return False
-            case.state = State.CLOSED
-            case_list.remove(case)
-            closed_list.append(case)
-            return True
-    interface.print_ticket_invalid_id(case_id)
-    return False
+    def close_ticket(self, case_id):
+        self.interface.print_close_ticket(case_id)
+        for case in self.backlog:
+            if case.id == case_id:
+                if case.responsible != Responsible.L1:
+                    self.interface.print_l1_close_ticket()
+                    return False
+                case.state = State.CLOSED
+                self.backlog.remove(case)
+                self.closed_backlog.append(case)
+                return True
+        self.interface.print_ticket_invalid_id(case_id)
+        return False
 
-
-# Ticket creation with id,customer name and description
-def create_ticket(id, name, description, type, case_list: list[Ticket]):
-    if not re.match("^Case-\d\d\d$", id):
-        interface.print_invalid_id()
-        return False
-    if len(description) == 0:
-        interface.print_invalid_details()
-        return False
-    if not re.match("^\w+$", name):
-        interface.print_invalid_name()
-        return False
-    type = Type.get_enum(type)
-    if type is None:
-        interface.print_invalid_type()
-        return False
-    for ticket in case_list:
-        if ticket.id == id:
-            interface.print_id_already_exists()
+    # Ticket creation with id,customer name and description
+    def create_ticket(self, id, name, description, ticket_type):
+        if not re.match("^Case-\d\d\d$", id):
+            self.interface.print_invalid_id()
             return False
-    ticket = Ticket(id, name, description, type, State.NEW, Responsible.L1)
-    interface.print_created_ticket(ticket)
-    case_list.append(ticket)
-    return True
+        if len(description) == 0:
+            self.interface.print_invalid_details()
+            return False
+        if not re.match("^\w+$", name):
+            self.interface.print_invalid_name()
+            return False
+        ticket_type_enum = Type.get_enum(ticket_type)
+        if ticket_type_enum is None:
+            self.interface.print_invalid_type()
+            return False
+        for ticket in self.backlog:
+            if ticket.id == id:
+                self.interface.print_id_already_exists()
+                return False
+        ticket = Ticket(
+            id, name, description, ticket_type_enum, State.NEW, Responsible.L1
+        )
+        self.interface.print_created_ticket(ticket)
+        self.backlog.append(ticket)
+        return True
 
+    # get an issue and print the details of it
+    def print_one_ticket(self, case_id):
+        for ticket in self.backlog:
+            if ticket.id == case_id:
+                self.interface.print_searched_ticket(ticket)
+                return True
+        self.interface.print_ticket_invalid_id(case_id)
+        return False
 
-# get a issue and print the details of it
-def print_one_ticket(case_id, case_list):
-    for ticket in case_list:
-        if ticket.id == case_id:
-            interface.print_searched_ticket(ticket)
-            return True
-    interface.print_ticket_invalid_id(case_id)
-    return False
+    # Get a keyword from user and search issues that contain that substring
+    def search_tickets(self, keyword):
+        found = False
+        self.interface.print_searched_keyword(keyword)
+        for ticket in self.backlog:
+            if (
+                keyword in ticket.id
+                or keyword in ticket.name
+                or keyword in ticket.type.value
+                or keyword in ticket.details
+                or keyword in ticket.state.value
+                or keyword in ticket.responsible.value
+            ):
+                self.interface.print_searched_ticket(ticket)
+                found = True
+        if not found:
+            self.interface.print_keyword_not_found(keyword)
+        return found
 
-
-# Get a keyword from user and search issues that contain that substring
-def search_tickets(keyword, case_list):
-    found = False
-    interface.print_searched_keyword(keyword)
-    for ticket in case_list:
-        if (
-            keyword in ticket.id
-            or keyword in ticket.name
-            or keyword in ticket.type.value
-            or keyword in ticket.details
-            or keyword in ticket.state.value
-            or keyword in ticket.responsible.value
+    # Update an issue in backlog
+    def update_ticket(self, case_id, new_state, new_assign):
+        new_state_enum = State.get_enum(new_state)
+        if new_state_enum is None or (
+            new_state_enum != State.ANALYSIS
+            and new_state_enum != State.SOLVED
+            and new_state_enum != State.IN_DELIVERY
         ):
-            interface.print_searched_ticket(ticket)
-            found = True
-    if not found:
-        interface.print_keyword_not_found(keyword)
-    return found
-
-
-# Update an issue in backlog
-def update_ticket(case_id, new_state, new_assign, ticketlist):
-    new_state_enum = State.get_enum(new_state)
-    if new_state_enum is None or (
-        new_state_enum != State.ANALYSIS
-        and new_state_enum != State.SOLVED
-        and new_state_enum != State.IN_DELIVERY
-    ):
-        interface.print_invalid_state(new_state)
+            self.interface.print_invalid_state(new_state)
+            return False
+        new_assign_enum = Responsible.get_enum(new_assign)
+        if new_state is None:
+            self.interface.print_invalid_responsible(new_assign)
+            return False
+        self.interface.print_updated_ticket(case_id, new_assign, new_state)
+        for ticket in self.backlog:
+            if ticket.id == case_id:
+                ticket.state = new_state_enum
+                ticket.responsible = new_assign_enum
+                return True
+        self.interface.print_ticket_invalid_id(case_id)
         return False
-    new_assign_enum = Responsible.get_enum(new_assign)
-    if new_state is None:
-        interface.print_invalid_responsible(new_assign)
-        return False
-    interface.print_updated_ticket(case_id, new_assign, new_state)
-    for ticket in ticketlist:
-        if ticket.id == case_id:
-            ticket.state = new_state_enum
-            ticket.responsible = new_assign_enum
-            return True
-    interface.print_ticket_invalid_id(case_id)
-    return False
 
+    def main(self):
+        # An infinite loop for menu that constantly asks user for their selection
+        # Does operations selected by the input
 
-def main():
-    # An infinite loop for menu that constantly asks user for their selection
-    # Does operations selected by the input
-    backlog = []
-    closed_backlog = []
+        while 1:
+            val = self.interface.print_main_form()
+            if val == "1":  # Create a ticket
+                id, name, description, ticket_type = (
+                    self.interface.print_form_create_ticket()
+                )
+                # Detect problems in createIssue function and display error message to user
 
-    while 1:
-        val = interface.print_main_form()
-        if val == "1":  # Create a ticket
-            id, name, description, type = interface.print_form_create_ticket()
-            # Detect problems in createIssue function and display error message to user
-
-            create_ticket(id, name, description, type, backlog)
-        elif val == "2":  # Assign a ticket
-            id, state, assign_name = interface.print_form_update_ticket()
-            update_ticket(id, state, assign_name, backlog)
-        elif val == "3":  # Close a ticket
-            id = interface.print_form_close_ticket()
-            close_ticket(id, backlog, closed_backlog)
-        elif val == "4":  # Search issues
-            keyword = interface.print_form_search_ticket()
-            search_tickets(keyword, backlog)
-        elif val == "5":  # Display issue
-            id = interface.print_one_form_ticket()
-            print_one_ticket(id, backlog)
-        elif val == "6":  # Sortie
-            break
-        else:
-            interface.print_invalid_selection()
+                self.create_ticket(id, name, description, ticket_type)
+            elif val == "2":  # Assign a ticket
+                id, state, assign_name = self.interface.print_form_update_ticket()
+                self.update_ticket(id, state, assign_name)
+            elif val == "3":  # Close a ticket
+                id = self.interface.print_form_close_ticket()
+                self.close_ticket(id)
+            elif val == "4":  # Search issues
+                keyword = self.interface.print_form_search_ticket()
+                self.search_tickets(keyword)
+            elif val == "5":  # Display issue
+                id = self.interface.print_one_form_ticket()
+                self.print_one_ticket(id)
+            elif val == "6":  # Sortie
+                break
+            else:
+                self.interface.print_invalid_selection()
--- test/test_tms.py
+++ test/test_tms.py
@@ -1,12 +1,14 @@
 import unittest
-from src import tms
+from src.tms import TMS
 from src.ticket import Ticket
 from src.constants import State, Responsible, Type
+from src.interface import Interface
 
 
 class CreateTicketTest(unittest.TestCase):
     def setUp(self):
-        self.backlog = []
+        self.tms = TMS(Interface())
+        self.tms.backlog = []
         self.ticket = Ticket(
             "Case-001",
             "IUT",
@@ -17,70 +19,65 @@ class CreateTicketTest(unittest.TestCase):
         )
 
     def test_creation(self):
-        tms.create_ticket(
+        self.tms.create_ticket(
             self.ticket.id,
             self.ticket.name,
             self.ticket.details,
             self.ticket.type.value,
-            self.backlog,
         )
-        self.assertTrue(self.ticket.__eq__(self.backlog[0]))
+        self.assertTrue(self.ticket.__eq__(self.tms.backlog[0]))
 
     def test_creation_invalid_id(self):
-        success = tms.create_ticket(
+        success = self.tms.create_ticket(
             "Case-",
             self.ticket.name,
             self.ticket.details,
             self.ticket.type.value,
-            self.backlog,
         )
-        self.assertEqual(self.backlog, [])
+        self.assertEqual(self.tms.backlog, [])
         self.assertFalse(success)
 
     def test_creation_wrong_type_field(self):
-        success = tms.create_ticket(
+        success = self.tms.create_ticket(
             self.ticket.id,
             self.ticket.name,
             self.ticket.details,
             "PRR",
-            self.backlog,
         )
 
-        self.assertEqual(self.backlog, [])
+        self.assertEqual(self.tms.backlog, [])
         self.assertFalse(success)
-        success = tms.create_ticket(
+        success = self.tms.create_ticket(
             self.ticket.id,
             self.ticket.name,
             self.ticket.details,
             422,
-            self.backlog,
         )
 
-        self.assertEqual(self.backlog, [])
+        self.assertEqual(self.tms.backlog, [])
         self.assertFalse(success)
 
     def test_same_id(self):
-        success = tms.create_ticket(
+        success = self.tms.create_ticket(
             self.ticket.id,
             self.ticket.name,
             self.ticket.details,
             self.ticket.type.value,
-            self.backlog,
         )
         self.assertTrue(success)
-        self.assertTrue(self.ticket.__eq__(self.backlog[0]))
-        success = tms.create_ticket(
+        self.assertTrue(self.ticket.__eq__(self.tms.backlog[0]))
+        success = self.tms.create_ticket(
             self.ticket.id,
             self.ticket.name,
             self.ticket.details,
             self.ticket.type.value,
-            self.backlog,
         )
         self.assertFalse(success)
 
 
 class UpdateTicketTest(unittest.TestCase):
     def setUp(self):
+        self.tms = TMS(Interface())
         self.ticket = Ticket(
             "Case-001",
             "IUT",
@@ -89,42 +86,38 @@ class UpdateTicketTest(unittest.TestCase):
             State.NEW,
             Responsible.L1,
         )
-        self.backlog = [self.ticket]
+        self.tms.backlog = [self.ticket]
 
     def test_update_ticket(self):
-        success = tms.update_ticket(
+        success = self.tms.update_ticket(
             self.ticket.id,
             State.ANALYSIS.value,
             Responsible.L2.value,
-            self.backlog,
         )
-        self.assertEqual(self.backlog[0].state, State.ANALYSIS)
-        self.assertEqual(self.backlog[0].responsible, Responsible.L2)
+        self.assertEqual(self.tms.backlog[0].state, State.ANALYSIS)
+        self.assertEqual(self.tms.backlog[0].responsible, Responsible.L2)
         self.assertTrue(success)
 
     def test_wrong_state(self):
-        success = tms.update_ticket(
-            self.ticket.id, "WRONG", Responsible.L2, self.backlog
-        )
-        self.assertEqual(self.backlog[0].state, State.NEW)
-        self.assertEqual(self.backlog[0].responsible, Responsible.L1)
-        self.assertNotEqual(self.backlog[0].state, "WRONG")
-        self.assertNotEqual(self.backlog[0].responsible, Responsible.L2)
+        success = self.tms.update_ticket(self.ticket.id, "WRONG", Responsible.L2)
+        self.assertEqual(self.tms.backlog[0].state, State.NEW)
+        self.assertEqual(self.tms.backlog[0].responsible, Responsible.L1)
+        self.assertNotEqual(self.tms.backlog[0].state, "WRONG")
+        self.assertNotEqual(self.tms.backlog[0].responsible, Responsible.L2)
         self.assertFalse(success)
 
     def test_wrong_responsible(self):
-        success = tms.update_ticket(
-            self.ticket.id, State.ANALYSIS, "WRONG", self.backlog
-        )
-        self.assertEqual(self.backlog[0].state, State.NEW)
-        self.assertEqual(self.backlog[0].responsible, Responsible.L1)
-        self.assertNotEqual(self.backlog[0].state, State.ANALYSIS)
-        self.assertNotEqual(self.backlog[0].responsible, "WRONG")
+        success = self.tms.update_ticket(self.ticket.id, State.ANALYSIS, "WRONG")
+        self.assertEqual(self.tms.backlog[0].state, State.NEW)
+        self.assertEqual(self.tms.backlog[0].responsible, Responsible.L1)
+        self.assertNotEqual(self.tms.backlog[0].state, State.ANALYSIS)
+        self.assertNotEqual(self.tms.backlog[0].responsible, "WRONG")
         self.assertFalse(success)
 
 
 class CloseTicketTest(unittest.TestCase):
     def setUp(self):
+        self.tms = TMS(Interface())
         self.ticket = Ticket(
             "Case-001",
             "IUT",
@@ -133,31 +126,31 @@ class CloseTicketTest(unittest.TestCase):
             State.NEW,
             Responsible.L1,
         )
-        self.backlog = [self.ticket]
-        self.closed_backlog = []
+        self.tms.backlog = [self.ticket]
 
     def test_close_ticket(self):
-        success = tms.close_ticket(self.ticket.id, self.backlog, self.closed_backlog)
-        self.assertEqual(self.closed_backlog[0], self.ticket)
-        self.assertEqual(self.backlog, [])
+        success = self.tms.close_ticket(self.ticket.id)
+        self.assertEqual(self.tms.closed_backlog[0], self.ticket)
+        self.assertEqual(self.tms.backlog, [])
         self.assertTrue(success)
 
     def test_close_ticket_invalid_id(self):
-        success = tms.close_ticket("Case-002", self.backlog, self.closed_backlog)
-        self.assertEqual(self.closed_backlog, [])
-        self.assertEqual(self.backlog[0], self.ticket)
+        success = self.tms.close_ticket("Case-002")
+        self.assertEqual(self.tms.closed_backlog, [])
+        self.assertEqual(self.tms.backlog[0], self.ticket)
         self.assertFalse(success)
 
     def test_wrong_responsible(self):
         self.ticket.responsible = Responsible.L2
-        success = tms.close_ticket(self.ticket.id, self.backlog, self.closed_backlog)
-        self.assertEqual(self.closed_backlog, [])
-        self.assertEqual(self.backlog[0], self.ticket)
+        success = self.tms.close_ticket(self.ticket.id)
+        self.assertEqual(self.tms.closed_backlog, [])
+        self.assertEqual(self.tms.backlog[0], self.ticket)
         self.assertFalse(success)
 
 
 class SearchTicketTest(unittest.TestCase):
     def setUp(self):
+        self.tms = TMS(Interface())
         self.ticket = Ticket(
             "Case-001",
             "IUT",
@@ -166,18 +159,18 @@ class SearchTicketTest(unittest.TestCase):
             State.NEW,
             Responsible.L1,
         )
-        self.backlog = [self.ticket]
+        self.tms.backlog = [self.ticket]
 
     def test_search_ticket_by_type(self):
-        found = tms.search_tickets(Type.PR.value, self.backlog)
+        found = self.tms.search_tickets(Type.PR.value)
         self.assertTrue(found)
 
     def test_search_ticket_by_state(self):
-        found = tms.search_tickets(State.NEW.value, self.backlog)
+        found = self.tms.search_tickets(State.NEW.value)
         self.assertTrue(found)
 
     def test_search_ticket_by_responsible(self):
-        found = tms.search_tickets(Responsible.L1.value, self.backlog)
+        found = self.tms.search_tickets(Responsible.L1.value)
         self.assertTrue(found)
 
 
