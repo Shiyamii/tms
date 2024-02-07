79ac118e69c1d10c06bf85d2dd1fdf2db325031f
Tamas Palotas <tamas.palotas@etu.univ-cotedazur.fr>
2024-02-07 12:03:29 +0100

    Setup gui create ticket

--- run_program.py
+++ run_program.py
@@ -6,7 +6,5 @@ from src.tms import TMS
 from src.db import DB
 
 if __name__ == "__main__":
-    gui = GUI()
-    gui.run()
-    # tms = TMS(Interface(), DB())
-    # tms.main()
+    tms = TMS(GUI(), DB())
+    tms.main()
--- src/gui.py
+++ src/gui.py
@@ -1,8 +1,11 @@
 from src.abstract_interface import AbstractInterface
 import PySimpleGUI as sg
 
+from src.constants import Type
+from src.ticket import Ticket
 
-class GUI:
+
+class GUI(AbstractInterface):
 
     def __init__(self):
         self.window = sg.Window("Ticket Management System", self.get_default_layout())
@@ -19,35 +22,11 @@ class GUI:
             [sg.Button("Exit", key="exit")],
         ]
 
-    def run(self):
-        self.window = None
-        self.window = sg.Window("Ticket Management System", self.get_default_layout())
 
-        while True:
-            event, values = self.window.read()
+    def print_close_ticket(self, case_id):
+        pass
 
-            if event in (sg.WINDOW_CLOSED, "exit"):
-                break
-            elif event == "create_ticket":
-                print("Creating a ticket")
-            elif event == "update_ticket":
-                print("Updating a ticket")
-            elif event == "close_ticket":
-                print("Closing a ticket")
-                result = self.print_form_close_ticket()
-                print("Close ticket", result)
-                self.window = sg.Window(
-                    "Ticket Management System", self.get_default_layout()
-                )
-            elif event == "search_keyword":
-                print("Searching keyword")
-            elif event == "display_issue":
-                print("Displaying issue")
-
-        self.window.close()
-
-    @staticmethod
-    def print_form_close_ticket():
+    def print_form_close_ticket(self):
         layout = [
             [sg.Text("Ticket Management System")],
             [sg.Text("Ticket-ID"), sg.Input(key="case_id")],
@@ -56,18 +35,140 @@ class GUI:
                 sg.Button("Cancel", key="cancel"),
             ],
         ]
-        window = sg.Window("Ticket Management System", layout)
+        self.window = sg.Window("Ticket Management System", layout)
         value = None
         running = True
         while running:
-            event, values = window.read()
+            event, values = self.window.read()
             if event in (sg.WIN_CLOSED, "cancel"):
                 running = False
             elif event == "Close ticket":
                 value = values["case_id"]
-                running = False
-        window.close()
+                if value == "":
+                    value = None
+                    sg.popup("Please enter a Ticket-ID")
+                else:
+                    running = False
+        self.window.close()
         return value
 
-    def button_go(self):
-        sg.popup("Go button clicked", "Input value:", self.values["-IN-"])
+    def print_l1_close_ticket(self):
+        pass
+
+    def print_created_ticket(self, ticket: Ticket):
+        sg.popup("Ticket created: ", ticket.id)
+        pass
+
+    def print_form_create_ticket(self):
+        layout = [
+            [sg.Text("Ticket Management System", size=(30, 1), font=("Helvetica", 25))],
+            [sg.Text("Create new ticket", size=(20, 1), font=("Helvetica", 20))],
+            [sg.Text("Ticket-ID"), sg.Input(key="case_id")],
+            [sg.Text("Customer name"), sg.Input(key="name")],
+            [sg.Text("Case description"), sg.Input(key="description")],
+            [sg.Text("Case type")],
+            [sg.Radio(Type.PR.value, "type", default=True, key=Type.PR.value), sg.Radio(Type.IR.value, "type", key=Type.IR.value)],
+            [
+                sg.Button("Create ticket", key="Create ticket"),
+                sg.Button("Cancel", key="cancel"),
+            ],]
+        self.window = sg.Window("Ticket Management System", layout)
+        case_id = None
+        name = None
+        description = None
+        ticket_type = None
+        running = True
+        while running:
+            event, values = self.window.read()
+            if event in (sg.WIN_CLOSED, "cancel"):
+                running = False
+            elif event == "Create ticket":
+                if values["case_id"] == "" or values["name"] == "" or values["description"] == "":
+                    sg.popup("Please fill all fields")
+                else:
+                    running = False
+                    case_id = values["case_id"]
+                    name = values["name"]
+                    description = values["description"]
+                    if values[Type.IR.value]:
+                        ticket_type = Type.IR.value
+                    else:
+                        ticket_type = Type.PR.value
+
+        self.window.close()
+        return case_id, name, description, ticket_type
+
+    def print_searched_ticket(self, ticket: Ticket):
+        pass
+
+    def print_ticket_invalid_id(self, case_id):
+        pass
+
+    def print_form_search_ticket(self):
+        pass
+
+    def print_searched_keyword(self, keyword):
+        pass
+
+    def print_keyword_not_found(self, keyword):
+        pass
+
+    def print_updated_ticket(self, case_id, new_assign, new_state):
+        pass
+
+    def print_form_update_ticket(self):
+        pass
+
+    def print_one_form_ticket(self):
+        pass
+
+    def print_main_form(self):
+        self.window = None
+        self.window = sg.Window("Ticket Management System", self.get_default_layout())
+
+        while True:
+            event, values = self.window.read()
+
+            if event in (sg.WINDOW_CLOSED, "exit"):
+                self.window.close()
+                return "6"
+            elif event == "create_ticket":
+                self.window.close()
+                return "1"
+            elif event == "update_ticket":
+                self.window.close()
+                return "2"
+            elif event == "close_ticket":
+                self.window.close()
+                return "3"
+            elif event == "search_keyword":
+                self.window.close()
+                return "4"
+            elif event == "display_issue":
+                self.window.close()
+                return "5"
+
+
+    def print_invalid_selection(self):
+        pass
+
+    def print_invalid_id(self):
+        pass
+
+    def print_invalid_name(self):
+        pass
+
+    def print_invalid_details(self):
+        pass
+
+    def print_invalid_type(self):
+        pass
+
+    def print_invalid_state(self, new_state):
+        pass
+
+    def print_invalid_responsible(self, new_assign):
+        pass
+
+    def print_id_already_exists(self):
+        pass
