f6cd8c53080f1260b651580a6cd5154155fdf4e8
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2024-03-13 19:24:25 +0100

    Add docstring for constants

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,28 @@
+"""
+This module defines the constants used in the project.
+
+It includes the following classes:
+- State: An enumeration representing the possible states of a ticket.
+- Responsible: An enumeration representing the possible responsible levels for a ticket.
+- Type: An enumeration representing the possible types of a ticket.
+"""
+
 from enum import Enum
 
 
 class State(Enum):
+    """
+    An enumeration representing the possible states of a ticket.
+
+    The states include:
+    - NEW
+    - ASSIGNED
+    - ANALYSIS
+    - SOLVED
+    - IN_DELIVERY
+    - CLOSED
+    """
+
     NEW = "new"
     ASSIGNED = "assigned"
     ANALYSIS = "analysis"
@@ -11,10 +32,22 @@ class State(Enum):
 
     @classmethod
     def has_value(cls, value):
+        """
+        Checks if the given value is a valid state.
+
+        :param value: The value to check.
+        :return: True if the value is a valid state, False otherwise.
+        """
         return value in cls._value2member_map_
 
     @classmethod
     def get_enum(cls, value):
+        """
+        Returns the enum member corresponding to the given value.
+
+        :param value: The value to get the enum member for.
+        :return: The enum member corresponding to the given value, or None if the value is not a valid state.
+        """
         if value == "new":
             return cls.NEW
         if value == "assigned":
@@ -31,16 +64,37 @@ class State(Enum):
 
 
 class Responsible(Enum):
+    """
+    An enumeration representing the possible responsible levels for a ticket.
+
+    The levels include:
+    - L1
+    - L2
+    - L3
+    """
+
     L1 = "L1"
     L2 = "L2"
     L3 = "L3"
 
     @classmethod
     def has_value(cls, value):
+        """
+        Checks if the given value is a valid responsible level.
+
+        :param value: The value to check.
+        :return: True if the value is a valid responsible level, False otherwise.
+        """
         return value in cls._value2member_map_
 
     @classmethod
     def get_enum(cls, value):
+        """
+        Returns the enum member corresponding to the given value.
+
+        :param value: The value to get the enum member for.
+        :return: The enum member corresponding to the given value, or None if the value is not a valid responsible level.
+        """
         if value == "L1":
             return cls.L1
         if value == "L2":
@@ -51,15 +105,35 @@ class Responsible(Enum):
 
 
 class Type(Enum):
+    """
+    An enumeration representing the possible types of a ticket.
+
+    The types include:
+    - PR
+    - IR
+    """
+
     PR = "PR"
     IR = "IR"
 
     @classmethod
     def has_value(cls, value):
+        """
+        Checks if the given value is a valid ticket type.
+
+        :param value: The value to check.
+        :return: True if the value is a valid ticket type, False otherwise.
+        """
         return value in cls._value2member_map_
 
     @classmethod
     def get_enum(cls, value):
+        """
+        Returns the enum member corresponding to the given value.
+
+        :param value: The value to get the enum member for.
+        :return: The enum member corresponding to the given value, or None if the value is not a valid ticket type.
+        """
         if value == "PR":
             return cls.PR
         if value == "IR":
