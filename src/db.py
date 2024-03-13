df007c97340d7cd8c14f1261b8c5a6e05e2a0327
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2024-03-13 19:37:11 +0100

    Add docstring for db

--- src/db.py
+++ src/db.py
@@ -7,12 +7,28 @@ from typing import Optional
 
 
 class DB(AbstractData):
+    """
+    This class represents the database operations for the ticket management system.
+    It provides methods to create, update, close, and search tickets in the database.
+    """
+
     def __init__(self, database_connection: DatabaseConnect):
+        """
+        Initialize the DB class with a database connection.
+
+        :param database_connection: An instance of DatabaseConnect class.
+        """
         self.database_connection = database_connection
         self.database_connection.connect()
 
     @staticmethod
     def data_to_ticket(data):
+        """
+        Convert raw data to a Ticket object.
+
+        :param data: A tuple containing ticket data.
+        :return: A Ticket object.
+        """
         return Ticket(
             data[0],
             data[1],
@@ -24,9 +40,21 @@ class DB(AbstractData):
         )
 
     def data_to_tickets(self, data):
+        """
+        Convert a list of raw data to a list of Ticket objects.
+
+        :param data: A list of tuples, each containing ticket data.
+        :return: A list of Ticket objects.
+        """
         return [self.data_to_ticket(ticket) for ticket in data]
 
     def search_tickets(self, keyword):
+        """
+        Search tickets in the database using a keyword.
+
+        :param keyword: A string to search in the database.
+        :return: A list of Ticket objects that match the keyword.
+        """
         query = """
             SELECT t.id,t.name,t.description,t.ticket_type,t.state,t.responsible,t.date_created 
             FROM ticket t 
@@ -52,6 +80,12 @@ class DB(AbstractData):
         return result
 
     def get_ticket(self, ticket_id) -> Optional[Ticket]:
+        """
+        Get a ticket from the database using its ID.
+
+        :param ticket_id: The ID of the ticket.
+        :return: A Ticket object if found, None otherwise.
+        """
         query = """
             SELECT t.id,t.name,t.description,t.ticket_type,t.state,t.responsible,t.date_created 
             FROM  ticket t 
@@ -64,12 +98,23 @@ class DB(AbstractData):
         return None
 
     def id_exists(self, ticket_id) -> bool:
+        """
+        Check if a ticket ID exists in the database.
+
+        :param ticket_id: The ID of the ticket.
+        :return: True if the ticket ID exists, False otherwise.
+        """
         query = "SELECT * FROM ticket WHERE id = %s AND state != %s"
         data = (ticket_id, State.CLOSED.value)
         result = self.database_connection.fetchone(query, data)
         return result is not None
 
     def create_ticket(self, ticket):
+        """
+        Create a new ticket in the database.
+
+        :param ticket: A Ticket object to be created in the database.
+        """
         query = """
             INSERT INTO ticket (id, name, description, ticket_type, state, responsible)
             VALUES (%s, %s, %s, %s, %s, %s);
@@ -85,6 +130,12 @@ class DB(AbstractData):
         self.database_connection.execute(query, data)
 
     def update_ticket(self, ticket):
+        """
+        Update an existing ticket in the database.
+
+        :param ticket: A Ticket object with updated data.
+        :return: True if the ticket was updated, False otherwise.
+        """
         if not self.id_exists(ticket.id):
             return False
         query = """
@@ -104,6 +155,12 @@ class DB(AbstractData):
         return True
 
     def close_ticket(self, ticket) -> bool:
+        """
+        Close an existing ticket in the database.
+
+        :param ticket: A Ticket object to be closed.
+        :return: True if the ticket was closed, False otherwise.
+        """
         if not self.id_exists(ticket.id):
             return False
         query = """
@@ -117,6 +174,11 @@ class DB(AbstractData):
         return True
 
     def get_old_new_ticket(self) -> list[Ticket]:
+        """
+        Get a list of new tickets that are older than 3 days.
+
+        :return: A list of Ticket objects.
+        """
         query = """
             SELECT t.id,t.name,t.description,t.ticket_type,t.state,t.responsible,t.date_created 
             FROM ticket t 
@@ -128,6 +190,11 @@ class DB(AbstractData):
         return result
 
     def get_old_assigned_ticket(self) -> list[Ticket]:
+        """
+        Get a list of assigned tickets that are older than 10 days.
+
+        :return: A list of Ticket objects.
+        """
         query = """
             SELECT t.id,t.name,t.description,t.ticket_type,t.state,t.responsible,t.date_created 
             FROM ticket t 
@@ -139,6 +206,11 @@ class DB(AbstractData):
         return result
 
     def get_old_ticket_list(self) -> list[Ticket]:
+        """
+        Get a list of all tickets that are older than 20 days.
+
+        :return: A list of Ticket objects.
+        """
         query = """
             SELECT t.id,t.name,t.description,t.ticket_type,t.state,t.responsible,t.date_created 
             FROM ticket t 
